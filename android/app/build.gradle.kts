5b58f2833f60401c9fb3347cd8ababaa97d24105
KaungZinLin <kaung.zl2011@gmail.com>
2026-02-10 13:58:35 +0630

    Fix gradle name errors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    id("com.kzl.super_scan")
+    id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.super_scan"
+    namespace = "com.kzl.super_scan"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.super_scan"
+        applicationId = "com.kzl.super_scan"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
